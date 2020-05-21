0d5389ecf986e4421ba5b44de745f58ca0fde54a
Jakub Biesiada <biesiadajakub@icloud.com>
2020-05-21 15:47:13 +0200

    Fix import error for selectMenu

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //import modules
 import disclosure from './dist/modules/disclosure';
-import selectMenu from './dist/modules/SelectMenu';
+import selectMenu from './dist/modules/selectMenu';
  
 export { disclosure, selectMenu };
